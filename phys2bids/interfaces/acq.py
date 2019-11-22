9f4ea24436daa292d04a0591f265ed05218cd025
smoia <s.moia@bcbl.eu>
2019-11-22 20:00:33 +0100

    removed unnecessary steps and corrected export error

--- phys2bids/interfaces/acq.py
+++ phys2bids/interfaces/acq.py
@@ -6,20 +6,20 @@ phys2bids interface for acqknowledge files.
 """
 from bioread import read_file
 
-from phys2bids.physio_obj import blueprint_input, blueprint_output
+from phys2bids.physio_obj import blueprint_input
 
 
 def populate_phys_input(filename, chtrig):
     """
     Populate object phys_input
-    """        
+    """
 
     data = read_file(filename).channels
-    
+
     freq = [data[chtrig].samples_per_second] * 2
     timeseries = [data[chtrig].time_index, data[chtrig].data]
     units = ['s', data[chtrig].units]
-    names = ['time','trigger']
+    names = ['time', 'trigger']
 
     k = 0
     for ch in data:
@@ -30,4 +30,4 @@ def populate_phys_input(filename, chtrig):
             units.append(ch.units)
         k += 1
 
-    return phys_input = blueprint_input(timeseries, freq, names, units)
+    return blueprint_input(timeseries, freq, names, units)
