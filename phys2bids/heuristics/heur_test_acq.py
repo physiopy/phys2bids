61b7f322dfbc69a94d65dc391d91aafae312ba00
smoia <s.moia@bcbl.eu>
2019-11-27 19:12:37 +0100

    Added heuristic for acq test file

---
+++ phys2bids/heuristics/heur_test_acq.py
@@ -0,0 +1,54 @@
+import sys
+import fnmatch
+
+
+def heur(physinfo, name, task='', acq='', direct='', rec='', run=''):
+    # ############################## #
+    # ##       Modify here!       ## #
+    # ##                          ## #
+    # ##  Possible variables are: ## #
+    # ##    -task (required)      ## #
+    # ##    -run                  ## #
+    # ##    -rec                  ## #
+    # ##    -acq                  ## #
+    # ##    -direct               ## #
+    # ##                          ## #
+    # ##                          ## #
+    # ##    See example below     ## #
+    # ############################## #
+
+    if fnmatch.fnmatchcase(physinfo, '*samefreq*.acq'):
+        task = 'test'
+        run = '00'
+        rec = 'biopac'
+    elif physinfo == 'Example':
+        task = 'rest'
+        run = '01'
+        acq = 'resp'
+        # ############################## #
+        # ## Don't modify below this! ## #
+        # ############################## #
+    else:
+        # #!# Transform sys.exit in debug warnings or raiseexceptions!
+        # #!# Make all of the above a dictionary
+        sys.exit()
+
+    if not task:
+        sys.exit()
+
+    name = name + '_task-' + task
+
+    # filename spec: sub-<label>[_ses-<label>]_task-<label>[_acq-<label>][_ce-<label>][_dir-<label>][_rec-<label>][_run-<index>][_recording-<label>]_physio
+    if acq:
+        name = name + '_acq-' + acq
+
+    if direct:
+        name = name + '_dir-' + direct
+
+    if rec:
+        name = name + '_rec-' + rec
+
+    if run:
+        name = name + '_run-' + run
+
+    return name
