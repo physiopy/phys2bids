e38b48aaa85c8b076c969b8fb94cfd1c2f92d031
eurunuela <enekouru@gmail.com>
2019-11-28 20:00:26 +0100

    Made a function for each test

--- phys2bids/tests/test_utils.py
+++ phys2bids/tests/test_utils.py
@@ -8,62 +8,90 @@ from phys2bids import utils
 
 
 # Tests check_input_dir
-test_path = '/home/mrmeeseeks/blue/'
-path_corrected = utils.check_input_dir(test_path)
-assert path_corrected == test_path[:-1]
+def test_check_input_dir():
+    test_path = '/home/mrmeeseeks/blue/'
+    path_corrected = utils.check_input_dir(test_path)
+    assert path_corrected == test_path[:-1]
+
 
 # Tests check_input_ext
-test_filename = 'help'
-ext = 'py'
-out_filename = utils.check_input_ext(test_filename, ext)
-assert str(out_filename) == f'{test_filename}.{ext}'
+def test_check_input_ext():
+    test_filename = 'help'
+    ext = 'py'
+    out_filename = utils.check_input_ext(test_filename, ext)
+    assert str(out_filename) == f'{test_filename}.{ext}'
+
 
 # Tests check_input_type
-test_filename = 'Test_belt_pulse_samefreq.acq'
-test_path = 'phys2bids/tests/data'
-assert utils.check_input_type(test_filename, test_path)
+def test_check_input_type():
+    test_filename = 'Test_belt_pulse_samefreq.acq'
+    test_path = 'phys2bids/tests/data'
+    assert utils.check_input_type(test_filename, test_path)
+
 
 # Tests path_exists_or_make_it
-test_folder = 'phys2bids/tests/foo'
-utils.path_exists_or_make_it(test_folder)
-os.path.isdir(test_folder)
-os.rmdir(test_folder) # Removes the created folder
+def test_path_exists_or_make_it():
+    test_folder = 'phys2bids/tests/foo'
+    utils.path_exists_or_make_it(test_folder)
+    os.path.isdir(test_folder)
+    os.rmdir(test_folder) # Removes the created folder
+
 
 # Tests check_file_exists
-test_full_path = os.path.join(test_path, test_filename)
-utils.check_file_exists(test_full_path)
+def test_check_file_exists():
+    test_filename = 'Test_belt_pulse_samefreq.acq'
+    test_path = 'phys2bids/tests/data'
+    test_full_path = os.path.join(test_path, test_filename)
+    utils.check_file_exists(test_full_path)
+
 
 # Tests move_file
-ext = '.txt'
-test_old_path = os.path.join(test_path, 'foo.txt')
-open(test_old_path, 'a').close()
-test_old_path = test_old_path[:-4]
-test_new_path = os.path.join(test_path, 'mrmeeseeks')
-utils.move_file(test_old_path, test_new_path, ext)
-os.remove(f'{test_new_path}.txt')
+def test_move_file():
+    ext = '.txt'
+    test_path = 'phys2bids/tests/data'
+    test_old_path = os.path.join(test_path, 'foo.txt')
+    open(test_old_path, 'a').close()
+    test_old_path = test_old_path[:-4]
+    test_new_path = os.path.join(test_path, 'mrmeeseeks')
+    utils.move_file(test_old_path, test_new_path, ext)
+    os.remove(f'{test_new_path}.txt')
+
 
 # Tests copy_file
-open(f'{test_old_path}.txt', 'a').close()
-utils.copy_file(test_old_path, test_new_path, ext)
-os.remove(f'{test_old_path}.txt')
-os.remove(f'{test_new_path}.txt')
+def test_copy_file():
+    ext = '.txt'
+    test_path = 'phys2bids/tests/data'
+    test_old_path = os.path.join(test_path, 'foo.txt')
+    test_new_path = os.path.join(test_path, 'mrmeeseeks')
+    open(f'{test_old_path}.txt', 'a').close()
+    utils.copy_file(test_old_path, test_new_path, ext)
+    os.remove(f'{test_old_path}.txt')
+    os.remove(f'{test_new_path}.txt')
+
 
 # Tests writefile
-test_text = 'Wubba lubba dub dub!'
-utils.writefile(test_old_path, ext, test_text)
-os.remove(f'{test_old_path}.txt')
+def test_writefile():
+    ext = '.txt'
+    test_path = 'phys2bids/tests/data'
+    test_old_path = os.path.join(test_path, 'foo.txt')
+    test_text = 'Wubba lubba dub dub!'
+    utils.writefile(test_old_path, ext, test_text)
+    os.remove(f'{test_old_path}.txt')
+
 
 # Tests writejson
-test_json_filename = os.path.join(test_path, 'foo')
-test_json_data = dict(SamplingFrequency=42,
-                      StartTime='00:00 ET',
-                      Columns='Rick')
-utils.writejson(test_json_filename, test_json_data, indent=4, sort_keys=False)
-os.remove(f'{test_json_filename}.json')
-
+def test_writejson():
+    test_path = 'phys2bids/tests/data'
+    test_json_filename = os.path.join(test_path, 'foo')
+    test_json_data = dict(SamplingFrequency=42,
+                        StartTime='00:00 ET',
+                        Columns='Rick')
+    utils.writejson(test_json_filename, test_json_data, indent=4, sort_keys=False)
+    os.remove(f'{test_json_filename}.json')
 
 # Tests load_heuristics
-heuristic_file = 'heur_test_acq.py'
-test_heuristic = os.path.join('phys2bids/heuristics', heuristic_file)
-heuristic_output_filename = utils.load_heuristic(test_heuristic).filename
-assert test_heuristic in heuristic_output_filename
+def test_load_heuristics():
+    heuristic_file = 'heur_test_acq.py'
+    test_heuristic = os.path.join('phys2bids/heuristics', heuristic_file)
+    heuristic_output_filename = utils.load_heuristic(test_heuristic).filename
+    assert test_heuristic in heuristic_output_filename
