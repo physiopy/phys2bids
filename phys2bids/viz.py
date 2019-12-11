1e1370d7b001aea54630cb4a5b241f0ad6e250c4
vinferrer <v.ferrer@bcbl.eu>
2019-12-11 11:08:38 +0100

    solve linting problems

--- phys2bids/viz.py
+++ phys2bids/viz.py
@@ -43,19 +43,19 @@ def plot_trigger(time, trigger, outfile, options, figsize=FIGSIZE, dpi=SET_DPI):
     plt.savefig(outfile + '_trigger_time.png', dpi=dpi)
     plt.close()
 
-def plot_all(phys_in, infile, outfile, dpi = SET_DPI, size = FIGSIZE):
-    ch_num = len(phys_in.ch_name) # get number of channels:
-    fig,ax = plt.subplots(ch_num-1, 1, figsize = size, sharex=True)
-    time = phys_in.timeseries[0] # assume time is first channel
+def plot_all(phys_in, infile, outfile, dpi=SET_DPI, size=FIGSIZE):
+    ch_num = len(phys_in.ch_name)  # get number of channels:
+    fig,ax = plt.subplots(ch_num - 1, 1, figsize=size, sharex=True)
+    time = phys_in.timeseries[0]  # assume time is first channel
     fig.suptitle(basename(infile))
     for row, timeser in enumerate(phys_in.timeseries[1:]):
-        if timeser.shape != time.shape :
-            timeser=resample(timeser.astype(float),time.shape[0]) 
-        ax[row].plot(time,timeser)
-        ax[row].set_title(f' Channel {row + 2}: {phys_in.ch_name[row+1]}')
+        if timeser.shape != time.shape:
+            timeser = resample(timeser.astype(float), time.shape[0])
+        ax[row].plot(time, timeser)
+        ax[row].set_title(f' Channel {row + 2}: {phys_in.ch_name[row + 1]}')
         ax[row].set_ylabel(phys_in.units[row +1])
-        ax[row].xlim=30*60*phys_in.freq[0] # maximum display of half an hour
+        ax[row].xlim = 30 * 60 * phys_in.freq[0]  # maximum display of half an hour
         ax[row].grid()
         row += 1
-    ax[row-1].set_xlabel("seconds")
-    fig.savefig(outfile, dpi = dpi, bbox_inches = 'tight')
+    ax[row - 1].set_xlabel("seconds")
+    fig.savefig(outfile, dpi=dpi, bbox_inches='tight')
