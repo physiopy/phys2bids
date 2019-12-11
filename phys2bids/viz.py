a9ccfaf9b879bc6ba80a64575c6c6b10b2eea52d
vinferrer <v.ferrer@bcbl.eu>
2019-12-11 10:24:03 +0100

    use only one index at for loop and name it row

--- phys2bids/viz.py
+++ phys2bids/viz.py
@@ -2,7 +2,7 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-
+from os.path import basename
 SET_DPI = 100
 FIGSIZE = (18, 10)
 
@@ -42,19 +42,18 @@ def plot_trigger(time, trigger, outfile, options, figsize=FIGSIZE, dpi=SET_DPI):
     plt.savefig(outfile + '_trigger_time.png', dpi=dpi)
     plt.close()
 
-def plot_all(phys_in,infile,outfile,dpi = SET_DPI,size = FIGSIZE):
+def plot_all(phys_in, infile, outfile, dpi = SET_DPI, size = FIGSIZE):
     ch_num = len(phys_in.ch_name) # get number of channels:
-    fig,ax = plt.subplots(ch_num-1,1,figsize = size,sharex=True)
-    row = 0
+    fig,ax = plt.subplots(ch_num-1, 1, figsize = size, sharex=True)
     time = phys_in.timeseries[0] # assume time is first channel
-    fig.suptitle(os.path.basename(infile))
-    for index, timeser in enumerate(phys_in.timeseries[1:]):
-        index += 1
+    fig.suptitle(basename(infile))
+    for row, timeser in enumerate(phys_in.timeseries[1:]):
+        if phys_in.freq[0] != phys_in.freq[row+1]:
         ax[row].plot(time,timeser)
-        ax[row].set_title(f' Channel {index + 1}: {phys_in.ch_name[index]}')
-        ax[row].set_ylabel(phys_in.units[index-1])
+        ax[row].set_title(f' Channel {row + 2}: {phys_in.ch_name[row+1]}')
+        ax[row].set_ylabel(phys_in.units[row +1])
         ax[row].xlim=30*60*phys_in.freq[0] # maximum display of half an hour
         ax[row].grid()
         row += 1
     ax[row-1].set_xlabel("seconds")
-    fig.savefig(outfile,dpi = dpi,bbox_inches = 'tight')
+    fig.savefig(outfile, dpi = dpi, bbox_inches = 'tight')
