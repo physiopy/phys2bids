482490f5fc9df645e018b3e175e49e47e8352d3e
vinferrer <v.ferrer@bcbl.eu>
2020-01-10 10:08:21 +0100

    acq_read function ready for pull request

--- phys2bids/interfaces/txt.py
+++ phys2bids/interfaces/txt.py
@@ -44,10 +44,10 @@ def populate_phys_input(filename, chtrig):
             channel_list.append(line)
         if len(header) == 0:
             raise AttributeError(f'Files without header are not supported yet')
-        if 'Interval=' in header[0]:
+        elif 'Interval=' in header[0]:
             print('phys2bids detected that your file is in labchart format')
             phys_in = labchart_read(channel_list, chtrig, header)
-        if 'acq' in header[0][0]:
+        elif 'acq' in header[0][0]:
             phys_in = acq_read(channel_list, chtrig, header)
         else:
             raise AttributeError(f'This file format is not supported yet for txt files')
@@ -95,9 +95,12 @@ def labchart_read(channel_list, chtrig, header=[]):
         interval[0] = float(interval[0])
     # get units
     range_list = header[5][1:]
-    units = []
+    orig_units = []
     for item in range_list:
-        units.append(item.split(' ')[1])
+        orig_units.append(item.split(' ')[1])
+    units = ['s', 'V']
+    orig_units.pop(chtrig - 1)
+    units = units + orig_units
     # get names
     orig_names = header[4][1:]
     names = ['time', 'trigger']
@@ -112,6 +115,7 @@ def labchart_read(channel_list, chtrig, header=[]):
     timeseries.pop(0)
     ordered_timeseries = ordered_timeseries + timeseries
     return BlueprintInput(ordered_timeseries, freq, names, units)
+
 
 def acq_read(channel_list, chtrig, header=[]):
     """
@@ -131,3 +135,50 @@ def acq_read(channel_list, chtrig, header=[]):
     # get frequency
     if len(header) == 0:
         raise AttributeError(f'Files without header are not supported yet')
+    interval = header[1][0].split()
+    if interval[-1].split('/')[0] not in ['hr', 'min', 'sec', 'µsec', 'msec']:
+        raise AttributeError(f'Interval unit "{interval[-1]}" is not in a valid LabChart'
+                             'time unit, this probably means your file is not in labchart format')
+    interval[-1] = interval[-1].split('/')[0]
+    if interval[-1] != 'sec':
+        print('Interval is not in seconds. Converting its value.')
+        if interval[-1] == 'hr':
+            interval[0] = float(interval)[0] * 3600
+            interval[-1] = 's'
+        elif interval[-1] == 'min':
+            interval[0] = float(interval[0]) * 60
+            interval[-1] = 's'
+        elif interval[-1] == 'msec':
+            interval[0] = float(interval[0]) / 1000
+            interval[-1] = 's'
+        elif interval[-1] == 'µs':
+            interval[0] = float(interval[0]) / 1000000
+            interval[-1] = 's'
+    else:
+        interval[0] = float(interval[0])
+        interval[-1] = 's'
+    # get units and names
+    orig_units = []
+    orig_names = []
+    for kj in range(3, len(header[-1]) + 8, 2):
+        orig_names.append(header[kj][0])
+        orig_units.append(header[kj + 1][0])
+    # reorder channels names
+    names = ['time', 'trigger']
+    orig_names.pop(chtrig - 1)
+    names = names + orig_names
+    # reoder channels units
+    units = ['s', 'Volts']
+    orig_units.pop(chtrig - 1)
+    units = units + orig_units
+    # get channels
+    timeseries = np.matrix(channel_list).T.tolist()
+    freq = [1 / interval[0]] * len(timeseries)
+    timeseries = [np.array(darray) for darray in timeseries]
+    duration = (timeseries[0].shape[0] + 1) * interval[0]
+    t_ch = np.ogrid[0:duration:interval[0]][:-1]  # create time channel
+    # step not equal to sample rate, check with stephano
+    ordered_timeseries = [t_ch, timeseries[chtrig - 1]]
+    timeseries.pop(chtrig - 1)
+    ordered_timeseries = ordered_timeseries + timeseries
+    return BlueprintInput(ordered_timeseries, freq, names, units)
