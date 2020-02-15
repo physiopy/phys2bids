da60f9796c6f6a8a41844f4d3f65c99867c9ccc1
vinferrer <v.ferrer@bcbl.eu>
2020-02-15 14:58:53 +0100

    add initial test to the parser

---
+++ phys2bids/tests/test_run.py
@@ -0,0 +1,15 @@
+import wget
+import os
+from pkg_resources import resource_filename
+
+
+def test_run():
+    url = 'https://osf.io/sdz4n/download'  # url to Test_belt_pulse_samefreq.txt
+    test_path = resource_filename('phys2bids', 'tests/data')
+    test_filename = 'Test_belt_pulse_samefreq.txt'
+    test_full_path = os.path.join(test_path, test_filename)
+    wget.download(url, test_full_path)
+    chtrig = 2
+    os.system('phys2bids -in ' + test_full_path + ' -info -indir '
+              + test_path + ' -chtrig ' + str(chtrig))
+    return
