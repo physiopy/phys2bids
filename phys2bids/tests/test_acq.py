6dbb7a18d8713b86f99c76cfde3c15f3b67b788d
eurunuela <enekouru@gmail.com>
2020-02-15 11:22:57 +0100

    Added test for biored. ACQ and bioread tests pass now.

--- phys2bids/tests/test_acq.py
+++ phys2bids/tests/test_acq.py
@@ -1,7 +1,8 @@
-from phys2bids.interfaces.acq import populate_phys_input
-from pkg_resources import resource_filename
 import os
 import wget
+from pkg_resources import resource_filename
+from bioread import read_file
+from phys2bids.interfaces.acq import populate_phys_input
 
 
 def test_populate_phys_input():
@@ -14,3 +15,23 @@ def test_populate_phys_input():
     wget.download(url, test_full_path)
     chtrig = 2
     phys_obj = populate_phys_input(test_full_path, chtrig)
+    assert phys_obj.ch_name[0] == 'time'
+    assert phys_obj.freq[0] == 10000.0
+    assert phys_obj.units[0] == 's'
+    # checkts that the trigger is in the given channel
+    assert phys_obj.ch_name[chtrig - 1] == 'trigger'
+    assert phys_obj.freq[chtrig - 1] == 10000.0
+    assert phys_obj.units[chtrig - 1] == 'Volts'
+
+
+def test_read_file():
+    # Makes sure that the read_file method from bioread works
+    # Read data to test
+    # url to Test_belt_pulse_samefreq.acq
+    url = 'https://osf.io/pn7vt//download'
+    test_path = resource_filename('phys2bids', 'tests/data')
+    test_filename = 'Test_belt_pulse_samefreq.txt'
+    test_full_path = os.path.join(test_path, test_filename)
+    wget.download(url, test_full_path)
+    data = read_file(test_full_path).channels
+    assert data[0].units == 'Volts'
