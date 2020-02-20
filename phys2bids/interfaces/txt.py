eda7998e2e92a00749341a7797a4063ea1559a68
vinferrer <v.ferrer@bcbl.eu>
2020-02-20 09:45:07 +0100

    eliminate -1 from acq_process chtrig

--- phys2bids/interfaces/txt.py
+++ phys2bids/interfaces/txt.py
@@ -186,7 +186,7 @@ def process_acq(channel_list, chtrig, header=[]):
         orig_units.append(header[index1 + 1][0])
     # reorder channels names
     names = ['time', 'trigger']
-    orig_names.pop(chtrig - 1)
+    orig_names.pop(chtrig)
     names = names + orig_names
     # reoder channels units
     units = ['s', 'Volts']
@@ -196,8 +196,8 @@ def process_acq(channel_list, chtrig, header=[]):
     timeseries = [np.array(darray) for darray in timeseries]
     duration = (timeseries[0].shape[0] + 1) * interval[0]
     t_ch = np.ogrid[0:duration:interval[0]][:-1]  # create time channel
-    ordered_timeseries = [t_ch, timeseries[chtrig - 1]]
-    timeseries.pop(chtrig - 1)
+    ordered_timeseries = [t_ch, timeseries[chtrig]]
+    timeseries.pop(chtrig)
     ordered_timeseries = ordered_timeseries + timeseries
     return BlueprintInput(ordered_timeseries, freq, names, units)
 
