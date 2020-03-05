53720584a5d40746868026f95a315db540c25317
vinferrer <v.ferrer@bcbl.eu>
2020-03-05 11:15:47 +0100

    add test function but leave it as a comment

--- phys2bids/tests/test_viz.py
+++ phys2bids/tests/test_viz.py
@@ -18,3 +18,16 @@ def test_plot_all():
     assert os.path.isfile(out)
     os.remove(test_full_path)
     os.remove(out)
+
+
+def test_plot_trigger():
+    url = 'https://osf.io/sdz4n/download'  # url to Test_belt_pulse_samefreq.txt
+    test_path = resource_filename('phys2bids', 'tests/data')
+    test_filename = 'Test_belt_pulse_samefreq.txt'
+    test_full_path = os.path.join(test_path, test_filename)
+    wget.download(url, test_full_path)
+    chtrig = 2
+    out = os.path.join(test_path, 'Test_belt_pulse_samefreq_trigger.png')
+    phys_obj = txt.populate_phys_input(test_full_path, chtrig)
+    viz.plot_trigger(phys_obj.timeseries[0], phys_obj.timeseries[1],
+                     , options)
