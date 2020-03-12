23c6cb667c125b98dee2218c11b72160164a990d
eurunuela <enekouru@gmail.com>
2020-03-12 10:31:51 +0100

    Renaming of a variable

--- phys2bids/tests/test_integration.py
+++ phys2bids/tests/test_integration.py
@@ -12,15 +12,15 @@ from phys2bids.phys2bids import phys2bids
 from phys2bids._version import get_versions
 
 
-def check_string(str_container, str_to_find, str_compare, is_num=True):
+def check_string(str_container, str_to_find, str_expected, is_num=True):
     idx = [log_idx for log_idx, log_str in enumerate(
                       str_container) if str_to_find in log_str]
     str_found = str_container[idx[0]]
     if is_num:
         num_found = re.findall(r"[-+]?\d*\.\d+|\d+", str_found)
-        return str_compare in num_found
+        return str_expected in num_found
     else:
-        return str_compare in str_found
+        return str_expected in str_found
 
 
 def test_logger():
