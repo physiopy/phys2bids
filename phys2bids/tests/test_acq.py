e12b0caff5e6a7629a262e360e3dce5ca81a22c1
Ross Markello <rossmarkello@gmail.com>
2020-03-19 21:01:55 -0400

    [TEST] Don't test bioread functionality
    That's implicit in `acq.populate_phys_input`

--- phys2bids/tests/test_acq.py
+++ phys2bids/tests/test_acq.py
@@ -1,5 +1,3 @@
-from bioread import read_file
-
 from phys2bids.interfaces.acq import populate_phys_input
 
 
@@ -17,9 +15,3 @@ def test_populate_phys_input(samefreq_full_acq_file):
     assert phys_obj.ch_name[chtrig] == 'MR TRIGGER - Custom, HLT100C - A 5'
     assert phys_obj.freq[chtrig] == 10000.0
     assert phys_obj.units[chtrig] == 'Volts'
-
-
-def test_read_file(samefreq_full_acq_file):
-    # Makes sure that the read_file method from bioread works
-    data = read_file(samefreq_full_acq_file).channels
-    assert data[0].units == 'Volts'
