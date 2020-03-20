4dfb3c88053147d955bba26b2acf081aa82cb430
Ross Markello <rossmarkello@gmail.com>
2020-03-19 20:52:16 -0400

    [TEST] Use .acq instead of .txt in test_viz
    Smaller file will hopefully make the tests a bit faster!

--- phys2bids/tests/test_viz.py
+++ phys2bids/tests/test_viz.py
@@ -1,23 +1,23 @@
 import os
 
-from phys2bids.interfaces import txt
+from phys2bids.interfaces import acq
 from phys2bids import viz
 
 
-def test_plot_all(samefreq_full_txt_file):
-    chtrig = 2
-    test_path, test_filename = os.path.split(samefreq_full_txt_file)
-    phys_obj = txt.populate_phys_input(samefreq_full_txt_file, chtrig)
+def test_plot_all(samefreq_full_acq_file):
+    chtrig = 3
+    test_path, test_filename = os.path.split(samefreq_full_acq_file)
+    phys_obj = acq.populate_phys_input(samefreq_full_acq_file, chtrig)
     out = os.path.join(test_path, 'Test_belt_pulse_samefreq.png')
     viz.plot_all(phys_obj, test_filename, outfile=out)
     assert os.path.isfile(out)
 
 
-def test_plot_trigger(samefreq_full_txt_file):
-    chtrig = 2
-    test_path, test_filename = os.path.split(samefreq_full_txt_file)
+def test_plot_trigger(samefreq_full_acq_file):
+    chtrig = 3
+    test_path, test_filename = os.path.split(samefreq_full_acq_file)
     out = os.path.join(test_path, 'Test_belt_pulse_samefreq')
-    phys_obj = txt.populate_phys_input(samefreq_full_txt_file, chtrig)
+    phys_obj = acq.populate_phys_input(samefreq_full_acq_file, chtrig)
     viz.plot_trigger(phys_obj.timeseries[0], phys_obj.timeseries[chtrig],
                      out, 1.5, 2.5, 0, test_filename)
     assert os.path.isfile(out + '_trigger_time.png')
