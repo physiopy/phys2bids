ca4394e55150bd831c73bc40fe77041b6273d1cc
vinferrer <v.ferrer@bcbl.eu>
2020-03-26 16:58:54 +0100

    change a bit log message

--- phys2bids/physio_obj.py
+++ phys2bids/physio_obj.py
@@ -272,7 +272,7 @@ class BlueprintInput():
         timepoints = trigger > thr
         num_timepoints_found = len([is_true for is_true, _ in groupby(timepoints,
                                     lambda x: x != 0) if is_true])
-        LGR.info(f'The number of expected timepoints according to the std method '
+        LGR.info(f'The number of timepoints according to the std method '
                  f'is {num_timepoints_found} and the threshold is {thr}')
         time_offset = self.timeseries[0][timepoints.argmax()]
 
