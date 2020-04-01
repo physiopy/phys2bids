23ffb63f91b144003fd566943e0fd7c816dd7391
vinferrer <v.ferrer@bcbl.eu>
2020-04-01 16:59:00 +0200

    add docstring to plot_all

--- phys2bids/viz.py
+++ phys2bids/viz.py
@@ -19,7 +19,7 @@ def plot_trigger(time, trigger, fileprefix, tr, thr, num_timepoints_expected,
     red line for the threshold used for trigger detection
     2. Same plot but this showing only the intial trigger
     3. Same plot but this showing only the intial trigger
-
+    More info in the howto.rst and doc page
     Parameters
     ----------
     time: numpy ndarray
@@ -127,6 +127,36 @@ def plot_trigger(time, trigger, fileprefix, tr, thr, num_timepoints_expected,
 
 
 def plot_all(ch_name, timeseries, units, freq, infile, outfile='', dpi=SET_DPI, size=FIGSIZE):
+    """
+    plots all the channels for visualizations and saves them in outfile
+    Parameters
+    ----------
+    ch_name : (ch) list of strings
+        List of names of the channels - can be the header of the columns
+        in the output files.
+    timeseries : (ch, [tps]) list
+        List of numpy 1d arrays - one for channel, plus one for time.
+        Time channel has to be the first, trigger the second.
+        Contains all the timeseries recorded.
+    units : (ch) list of strings
+        List of the units of the channels.
+    freq : (ch) list of floats
+        List of floats - one per channel.
+        Contains all the frequencies of the recorded channel.
+    infile: string
+        name of the input file to phys2bids
+    outfile: string
+        path of the output plot
+    dpi: int
+        Desired DPI of the figure (see `matplotlib`),
+        Default is {SET_DPI}
+    figsize: integer tuple or list
+        Size of the figure X*Y
+        Default is {FIGSIZE}
+    -----
+    outfile:
+        Creates new plot `fileprefix_trigger_time.png`.
+    """
     ch_num = len(ch_name)  # get number of channels:
     fig, ax = plt.subplots(ch_num - 1, 1, figsize=size, sharex=True)
     time = timeseries[0]  # assume time is first channel
