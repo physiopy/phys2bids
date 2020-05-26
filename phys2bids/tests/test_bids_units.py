3ca50f147949929d90726b076308f7c45bbe1608
vinferrer <v.ferrer@bcbl.eu>
2020-05-26 09:46:26 +0200

    adding test file

---
+++ phys2bids/tests/test_bids_units.py
@@ -0,0 +1,12 @@
+from phys2bids.bids_units import bidsify_units
+
+
+def test_bidsify_units():
+    # test normal unit
+    bidsify_units("V")
+    # test unit with standard prefix
+    bidsify_units("milik")
+    # test unit with not standard prefix
+    bidsify_units("matV")
+    # test unit that's not bids standard
+    bidsify_units("mmlie")
