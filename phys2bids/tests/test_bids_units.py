e2de3394d162acd779d3b2fecd475ea72af1b0fb
vinferrer <v.ferrer@bcbl.eu>
2020-05-29 09:07:17 +0200

    uppercase asserts

--- phys2bids/tests/test_bids_units.py
+++ phys2bids/tests/test_bids_units.py
@@ -3,10 +3,10 @@ from phys2bids.bids_units import bidsify_units
 
 def test_bidsify_units():
     # test normal unit
-    assert bidsify_units('V') == 'v'
+    assert bidsify_units('V') == 'V'
     # test unit with standard prefix
-    assert bidsify_units('centik') == 'ck'
+    assert bidsify_units('centik') == 'cK'
     # test unit with not standard prefix
-    assert bidsify_units('matV') == 'matv'
+    assert bidsify_units('matV') == 'matV'
     # test unit that's not bids standard
     assert bidsify_units('mmlie') == 'mmlie'
