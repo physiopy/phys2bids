b723beacc8dc384b34f7cc06b744ea628f9d6ec0
vinferrer <v.ferrer@bcbl.eu>
2020-05-29 09:25:18 +0200

    adding assert

--- phys2bids/tests/test_bids_units.py
+++ phys2bids/tests/test_bids_units.py
@@ -1,12 +1,14 @@
 from phys2bids.bids_units import bidsify_units
+from phys2bids.bids_units import unit_aliases
 
 
 def test_bidsify_units():
-    # test normal unit
-    assert bidsify_units('V') == 'V'
     # test unit with standard prefix
     assert bidsify_units('centik') == 'cK'
     # test unit with not standard prefix
     assert bidsify_units('matV') == 'matV'
     # test unit that's not bids standard
     assert bidsify_units('mmlie') == 'mmlie'
+    # test there is not problem with every unit in the dict
+    for unit_key in unit_aliases.keys():
+        assert bidsify_units(unit_key) == unit_aliases[unit_key]
