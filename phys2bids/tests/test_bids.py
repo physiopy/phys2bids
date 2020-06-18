881ab98493525e0144f7d7f777473b4f35a6e8b5
Vicente Ferrer <v.ferrer@bcbl.eu>
2020-06-18 12:10:06 +0200

    Update phys2bids/tests/test_bids.py
    lower case function in call test_bids.py
    
    Co-authored-by: Eneko Uruñuela <13706448+eurunuela@users.noreply.github.com>

--- phys2bids/tests/test_bids.py
+++ phys2bids/tests/test_bids.py
@@ -62,7 +62,7 @@ def test_use_heuristic(tmpdir, test_sub, test_ses):
 
 @pytest.mark.parametrize('outdir', '.')
 def test_README_file(outdir):
-    README_file(outdir)
+    readme_file(outdir)
     assert os.path.join(outdir, "README")
     os.remove(os.path.join(outdir, "README"))
 
