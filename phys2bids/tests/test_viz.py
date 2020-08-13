e329ab5d0e4da38b6f6e9b07f06909595d995f1d
Eneko <enekouru@gmail.com>
2020-08-13 14:08:10 +0200

    Fix test

--- phys2bids/tests/test_viz.py
+++ phys2bids/tests/test_viz.py
@@ -20,5 +20,5 @@ def test_plot_trigger(samefreq_full_acq_file):
     out = os.path.join(test_path, 'Test_belt_pulse_samefreq')
     phys_obj = acq.populate_phys_input(samefreq_full_acq_file, chtrig)
     viz.plot_trigger(phys_obj.timeseries[0], phys_obj.timeseries[chtrig],
-                     out, 1.5, 2.5, 0, test_filename)
+                     out, 1.5, 1.6, 60, test_filename)
     assert os.path.isfile(out + '_trigger_time.png')
