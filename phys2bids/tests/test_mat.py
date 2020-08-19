12e4829e4ef557f447eae60b21f1af71c05b308e
Eneko <enekouru@gmail.com>
2020-08-19 17:07:01 +0200

    Add unit test for MATLAB interface

---
+++ phys2bids/tests/test_mat.py
@@ -0,0 +1,21 @@
+from phys2bids.tests.conftest import matlab_file
+from phys2bids.interfaces.mat import populate_phys_input
+
+
+def test_populate_phys_input(matlab_file):
+    # Read data to test
+    chtrig = 1
+    phys_obj = populate_phys_input(matlab_file, chtrig)
+
+    # Check channel names are the same.
+    orig_channels = ['time', 'Trigger', 'CO2', 'O2', 'Belt', 'Pulse']
+    assert phys_obj.ch_name == orig_channels
+
+    # Check frequencies are the same.
+    orig_freq = [1000.0, 1000.0, 100.0, 40.0, 400.0, 1000.0]
+    assert phys_obj.freq == orig_freq
+
+    # Check units are the same
+    orig_units = ['s', 'V', 'mmHg', 'mmHg', 'V', 'V']
+    assert phys_obj.units == orig_units
+
