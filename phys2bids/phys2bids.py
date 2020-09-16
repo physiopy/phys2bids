3eaddb0cfbb56137ca0e9faf3211db46a1206060
vinferrer <v.ferrer@bcbl.eu>
2020-09-16 09:16:51 +0200

    use resampled time already calculated in a previus step

--- phys2bids/phys2bids.py
+++ phys2bids/phys2bids.py
@@ -381,10 +381,7 @@ def phys2bids(filename, info=False, indir='.', outdir='.', heur_file=None,
             if uniq_freq != phys_in[run].freq[chtrig]:
                 phys_out[key].ch_name.insert(1, phys_in[run].ch_name[chtrig])
                 phys_out[key].units.insert(1, phys_in[run].units[chtrig])
-                resampled_time = np.linspace(phys_in[run].timeseries[0][0],
-                                             phys_in[run].timeseries[0][-1],
-                                             num=phys_out[key].timeseries[0].shape[0])
-                phys_out[key].timeseries.insert(1, np.interp(resampled_time,
+                phys_out[key].timeseries.insert(1, np.interp(phys_out[key].timeseries[0],
                                                              phys_in[run].timeseries[0],
                                                              phys_in[run].timeseries[chtrig]))
             phys_out[key] = BlueprintOutput.init_from_blueprint(phys_out[key])
