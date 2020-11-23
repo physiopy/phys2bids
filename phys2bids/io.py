6944bae82069c03a14269faafd3345d5ebc25ecc
Vicente Ferrer <v.ferrer@bcbl.eu>
2020-11-23 14:27:24 +0100

    Update phys2bids/io.py
    Co-authored-by: Stefano Moia <s.moia@bcbl.eu>

--- phys2bids/io.py
+++ phys2bids/io.py
@@ -161,7 +161,7 @@ def generate_blueprint(channel_list, chtrig, interval, orig_units, orig_names):
     timeseries = list(map(list, zip(*channel_list)))
     freq = [1 / interval[0]] * len(timeseries)
     timeseries = [np.array(darray) for darray in timeseries]
-    # check the file has a time channel if not create it and add it
+    # Check if the file has a time channel, otherwise create it.
     # As the "time" doesn't have a column header, if the number of header names
     # is less than the number of timesieries, then "time" is column 0...
     # ...otherwise, create the time channel
