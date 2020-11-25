5f99e95168afe60aa5ae0228d54229d61d934c1a
Vicente Ferrer <v.ferrer@bcbl.eu>
2020-11-25 14:03:48 +0100

    0 isn't valid also

--- phys2bids/phys2bids.py
+++ phys2bids/phys2bids.py
@@ -191,7 +191,7 @@ def phys2bids(filename, info=False, indir='.', outdir='.', heur_file=None,
     # Check options to make them internally coherent pt. II
     # #!# This can probably be done while parsing?
     indir = utils.check_input_dir(indir)
-    if chtrig < 0:
+    if chtrig <= 0:
         raise Exception('Wrong trigger channel. Channel indexing starts with 1!')
 
     filename, ftype = utils.check_input_type(filename,
