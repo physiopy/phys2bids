59bd18f3f78492887e3cfac766270f3c7325bf9f
Vicente Ferrer <v.ferrer@bcbl.eu>
2020-11-26 12:51:59 +0100

    deleting copy for line 459

--- phys2bids/physio_obj.py
+++ phys2bids/physio_obj.py
@@ -456,7 +456,7 @@ class BlueprintInput():
         # Use the trigger channel to find the TRs,
         # comparing it to a given threshold.
         trigger = self.timeseries[self.trigger_idx]
-        time = self.timeseries[0].copy()
+        time = self.timeseries[0]
         LGR.info(f'The trigger is in channel {self.trigger_idx}')
         # Check that trigger and time channels have the same length.
         # If not, resample time to the length of the trigger
