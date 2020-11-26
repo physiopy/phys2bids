7a2d15687d0b21970e1e2676ba68fc80ee8fcd12
Vicente Ferrer <v.ferrer@bcbl.eu>
2020-11-26 16:58:13 +0100

    Update phys2bids/tests/test_physio_obj.py
    Co-authored-by: Taylor Salo <tsalo006@fiu.edu>

--- phys2bids/tests/test_physio_obj.py
+++ phys2bids/tests/test_physio_obj.py
@@ -103,6 +103,7 @@ def test_BlueprintInput():
     new_names = ['trigger', 'time', 'lindt']
     blueprint_in.rename_channels(new_names)
     assert blueprint_in.ch_name == ['time', 'trigger', 'lindt']
+    assert test_index[0] is not test_trigger
 
     # Tests return_index
     test_index = blueprint_in.return_index(1)
