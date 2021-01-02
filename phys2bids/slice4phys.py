6ab2115ba2f3c701008477f3e7f6319c791471d0
Taylor Salo <tsalo006@fiu.edu>
2021-01-01 19:16:55 -0500

    Add slice4phys.slice_phys.

--- phys2bids/slice4phys.py
+++ phys2bids/slice4phys.py
@@ -4,6 +4,9 @@ import logging
 from copy import deepcopy
 
 import numpy as np
+from scipy.signal import resample
+
+from phys2bids.bids import update_bids_name
 
 LGR = logging.getLogger(__name__)
 
@@ -104,6 +107,122 @@ def find_runs(phys_in, ntp_list, tr_list, thr=None, padding=9):
         phys_in = deepcopy(phys_in[update:-1])
 
     return run_timestamps
+
+
+def slice_phys(phys, run_timestamps, padding=9, update_trigger=False):
+    """Slice a physio object based on run-/file-wise onsets and offsets.
+
+    Parameters
+    ----------
+    phys : BlueprintInput
+    run_timestamps : dict
+        Each key is a run-wise filename and value is a tuple of (onset, offset),
+        where onset and offset are integers corresponding to index values of
+        the trigger channel.
+    padding : float or tuple, optional
+        Amount of time before and after run to keep in physio time series, in seconds.
+        May be a single value (in which case the time before and after is the same) or
+        a two-item tuple (which case the first item is time before and the second is
+        time after).
+        These values will be automatically reduced in cases where the pad would extend
+        before or after the physio acquisition.
+    update_trigger : bool, optional
+        Whether to update the trigger channel time series based on estimated scan onsets from
+        the BIDS dataset (True) or to leave it as-is (False). Default is False.
+
+    Returns
+    -------
+    phys_in_slices : dict
+        Each key is a run-wise filename (possibly further split by frequency)
+        and each value is a BlueprintInput object.
+
+    Notes
+    -----
+    The goal of this function is to abstract out the general slicing procedure
+    from slice4phys.
+    """
+    phys = deepcopy(phys)
+    if update_trigger:
+        phys.freq.append(phys.freq[phys.trigger_idx])
+        phys.units.append(phys.units[phys.trigger_idx])
+        phys.timeseries.append(phys.timeseries[phys.trigger_idx].copy())
+        phys.ch_name.append('original trigger')
+
+        # Fix up the trigger time series
+        phys.timeseries[phys.trigger_idx][:] = 0
+        for ons, off in run_timestamps.values():
+            phys.timeseries[phys.trigger_idx][ons:off] = 1
+
+    if not isinstance(padding, tuple):
+        time_before, time_after = padding, padding
+    else:
+        time_before, time_after = padding
+
+    phys_in_slices = {}
+    for i_run, fname in enumerate(run_timestamps.keys()):
+        run_attributes = run_timestamps[fname]  # tmp variable to collect run's info
+        trigger_onset, trigger_offset = run_attributes
+        min_onset, max_offset = 0, phys.timeseries[phys.trigger_idx].shape[0]
+
+        unique_frequencies = np.unique(phys.freq)
+        trigger_freq = phys.freq[phys.trigger_idx]
+
+        # Limit padding based on beginning and end of physio recording.
+        # We could also limit padding to prevent overlap between scans, if desired.
+        run_time_before = np.minimum(time_before, (trigger_onset - min_onset) / trigger_freq)
+        run_time_after = np.minimum(time_after, (max_offset - trigger_offset) / trigger_freq)
+
+        for freq in unique_frequencies:
+            to_subtract = int(run_time_before * freq)
+            to_add = int(run_time_after * freq)
+
+            run_onset_idx = int(trigger_onset * freq / trigger_freq) - to_subtract
+            run_offset_idx = int(trigger_offset * freq / trigger_freq) + to_add
+
+            # Split into frequency-specific object limited to onset-offset
+            temp_phys_in = deepcopy(phys[run_onset_idx:run_offset_idx])
+
+            if temp_phys_in.freq[temp_phys_in.trigger_idx] != freq:
+                example_ts = phys.timeseries[phys.freq.index(freq)]
+
+                # Determine time
+                new_time = (np.arange(example_ts.shape[0]) / temp_phys_in.timeseries[
+                    phys.freq.index(freq)].freq
+                )
+                new_time += np.min(temp_phys_in.timeseries[0])
+                temp_phys_in.timeseries[0] = new_time
+                temp_phys_in.freq[0] = freq
+
+                # Resample trigger
+                new_trigger = temp_phys_in.timeseries[temp_phys_in.trigger_idx]
+                new_trigger = resample(new_trigger, example_ts.shape[0], window=10)
+                temp_phys_in.timeseries[temp_phys_in.trigger_idx] = new_trigger
+                temp_phys_in.freq[temp_phys_in.trigger_idx] = freq
+
+            if len(unique_frequencies) > 1:
+                run_fname = update_bids_name(fname, recording=str(freq) + 'Hz')
+
+                # Drop other frequency channels
+                channel_idx = np.arange(temp_phys_in.ch_amount)
+                nonfreq_channels = [i for i in channel_idx if temp_phys_in.freq[i] != freq]
+                freq_channels = [i for i in channel_idx if temp_phys_in.freq[i] == freq]
+                temp_phys_in = temp_phys_in.delete_at_index(nonfreq_channels)
+
+                # Update trigger channel index around dropped channels
+                new_trigger_idx = freq_channels.index(temp_phys_in.trigger_idx)
+                temp_phys_in.trigger_idx = new_trigger_idx
+            else:
+                run_fname = fname
+
+            # zero out time
+            temp_phys_in.timeseries[0] = (
+                temp_phys_in.timeseries[0] - np.min(temp_phys_in.timeseries[0])
+            )
+            # Now take out the time before the scan starts
+            temp_phys_in.timeseries[0] = temp_phys_in.timeseries[0] - time_before
+
+            phys_in_slices[run_fname] = temp_phys_in
+    return phys_in_slices
 
 
 def slice4phys(phys_in, ntp_list, tr_list, thr, padding=9):
