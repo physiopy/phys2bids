408c63db78fd633bc4fbeb789e947639023c5812
Vicente Ferrer <v.ferrer@bcbl.eu>
2021-02-16 12:20:06 +0100

    Update phys2bids/physio_obj.py
    Co-authored-by: Stefano Moia <s.moia@bcbl.eu>

--- phys2bids/physio_obj.py
+++ phys2bids/physio_obj.py
@@ -585,7 +585,7 @@ class BlueprintInput():
         elif len(indexes) > 1:
             raise Exception('More than one possible trigger channel was automatically found. '
                             'Please run phys2bids specifying the -chtrig argument.')
-        if len(indexes) < 1:
+        else:
             raise Exception('No trigger channel automatically found. Please run phys2bids '
                             'specifying the -chtrig argument.')
 
